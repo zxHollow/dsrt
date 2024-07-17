b551d39c406e9b8b1a330808b1fd8fd02619e71c
zxHollow <dracotemporum@gmail.com>
2024-07-17 12:35:13 -0300

    v1.3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function text() {
-  textWin = window.open("add/text.html", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
+  textWin = window.open("text.html", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
 }
 
 function table() {
-  tableWin = window.open("add/table.html", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
+  tableWin = window.open("table.html", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
 }
 
 function book() {
