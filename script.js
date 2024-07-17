5a21cb385e7cb46165a4a6df5e210e31143c806b
zxHollow <dracotemporum@gmail.com>
2024-07-16 21:04:54 -0300

    v0.2

---
+++ script.js
@@ -0,0 +1,7 @@
+function dice() {
+    window.open("https://g.co/kgs/rtnCC1j", "_blank", "toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
+}
+
+  function table() {
+    window.open("table.html", "_blank", "toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
+ }
