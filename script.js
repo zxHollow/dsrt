c0c6d4dc63349dbcbb2850a5b286249fc3fe34ee
zxHollow <dracotemporum@gmail.com>
2024-07-17 12:14:48 -0300

    v1.1

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 function text() {
-  textWin = window.open("text.html", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
+  textWin = window.open("add/text.html", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
 }
 
 function table() {
-  tableWin = window.open("table.html", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
-}
-
-function phone() {
-  phoneWin = window.open("https://youtube.com/playlist?list=PLmOldskd2VbL7_t-NE9p6rEboq_v0AHko&si=aGx3Ime1mGKubYjM", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=370");
+  tableWin = window.open("add/table.html", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
 }
 
 function book() {
-  bookWin = window.open("books.html", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
+  bookWin = window.open("add/books.html", "_blank", "location=0, menubar= 0, toolbar=0,scrollbars=0,resizable=0,top=100,left=500,width=400,height=400");
 }
 
 function closeall() {
   textWin.close();
   tableWin.close();
-  phoneWin.close();
   bookWin.close();
 }
